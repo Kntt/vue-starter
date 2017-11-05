6c0407f5d98d5820fd5820a9c884ba8d9e2acf64
kntt <kntt@gmail.com>
2017-11-05 23:06:48 +0800

    add container

---
+++ src/components/container.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container" :class="{'scroll': scroll}" :style="{'background-color': bg, 'margin-top': ios ? '64px' : '44px'}">
+    <slot></slot>
+  </div>
+</template>
+<style lang="less" scoped>
+  .container {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+    &.scroll {
+      overflow: scroll;
+      -webkit-overflow-scrolling: touch;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+  }
+</style>
+<script>
+  export default{
+    name: 'container',
+    props: {
+      bg: { // 背景色
+        type: String,
+        default: '#F9F9F9'
+      },
+      scroll: { // 溢出滚动
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      ios () {
+        return this.$deviceInfo.ios
+      }
+    }
+  }
+</script>
