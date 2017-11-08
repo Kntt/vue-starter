8ae498f0420ed821dedfa2e33216ed131cfeeabe
kntt <kntt@gmail.com>
2017-11-08 23:57:51 +0800

    增加： loading， bubble组件

---
+++ src/components/loading/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="f-loading-container">
+    <img src="./loading.gif">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'loading'
+  }
+</script>
+
+<style lang="less">
+  .f-loading-container{
+    img {
+      width: 20px;
+      height: 20px;
+      display: block;
+    }
+  }
+</style>
