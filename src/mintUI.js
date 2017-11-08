ede7587bf629499de1f806032ba3200406706565
kntt <kntt@gmail.com>
2017-11-08 22:18:47 +0800

    修改：精简mint-ui

---
+++ src/mintUI.js
@@ -0,0 +1,8 @@
+/* ========================== mint-ui ============================ */
+import Vue from 'vue'
+import { Header, Button, Tabbar, TabItem } from 'mint-ui'
+
+Vue.component(Header.name, Header)
+Vue.component(Button.name, Button)
+Vue.component(Tabbar.name, Tabbar)
+Vue.component(TabItem.name, TabItem)
