065667650fec3cc51fabcb43b6fd3e04905e9821
kntt <kntt@gmail.com>
2017-11-09 23:17:24 +0800

    update postcss

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,8 +1,6 @@
 'use strict'
 const utils = require('./utils')
 const config = require('../config')
-const px2rem = require('postcss-px2rem-exclude')
-const autoprefixer = require('autoprefixer')
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
@@ -12,18 +10,6 @@ module.exports = {
       : config.dev.cssSourceMap,
     extract: isProduction
   }),
-  postcss: [
-    px2rem({
-      remUnit: 75,
-      exclude: /node_modules/
-    }),
-    autoprefixer({
-      browsers: [
-        'iOS >= 8',
-        'Android >= 4'
-      ]
-    })
-  ],
   transformToRequire: {
     video: 'src',
     source: 'src',
