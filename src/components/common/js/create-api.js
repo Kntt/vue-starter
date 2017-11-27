fc350e43105c362d5fea2962f47c194c749f9c0d
kntt <kntt@gmail.com>
2017-11-27 22:24:53 +0800

    updata common api

---
+++ src/components/common/js/create-api.js
@@ -0,0 +1,10 @@
+import { camelize } from '../lang/string'
+import createAPIComponent from './create-api-component'
+
+export default function createAPI (Vue, Component, events, single) {
+  const api = createAPIComponent.apply(this, arguments)
+  const name = Component.name
+  const createName = `$${camelize(`create-${name}`)}`
+  Vue.prototype[createName] = api.create
+  return api
+}
