a9a6cc2fbd549e2e1da9254dba406f2767d13154
kntt <kntt@gmail.com>
2017-11-27 22:56:26 +0800

    update loading

--- src/components/loading/index.vue
+++ src/components/loading/index.vue
@@ -1,21 +1,78 @@
 <template>
-  <div class="f-loading-container">
-    <img src="./loading.gif">
+  <div class="loading">
+    <span class="loading-spinners" :style="style">
+      <i class="loading-spinner" v-for="item in balde"></i>
+    </span>
   </div>
 </template>
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'loading'
 
-<script>
   export default {
-    name: 'loading'
-  }
-</script>
-
-<style lang="less">
-  .f-loading-container{
-    img {
-      width: 20px;
-      height: 20px;
-      display: block;
+    name: COMPONENT_NAME,
+    data () {
+      return {
+        balde: 12
+      }
+    },
+    props: {
+      size: {
+        type: Number,
+        default: 24
+      }
+    },
+    computed: {
+      style () {
+        const value = `${this.size}px`
+        return {
+          width: value,
+          height: value
+        }
+      }
     }
   }
+</script>
+<style lang="less">
+  // less 使用mixin做循环
+  .loop(@counter) when (@counter > 0) {
+    .loop((@counter - 1));
+    &:nth-child(@{counter}) {
+      animation-delay: ((@counter - 1) / 12)s;
+      transform: rotate(30deg * (@counter - 6)) translateY(-150%);
+    }
+  }
+
+  .loading {
+    font-size: 0;
+  }
+  .loading-spinners {
+    position: relative;
+    display: inline-block;
+    width: 20px; /*no*/
+    height: 20px; /*no*/
+  }
+  .loading-spinner {
+    position: absolute;
+    left: 44.5%;
+    top: 37%;
+    width: 10%;
+    height: 25%;
+    border-radius: 50%/20%;
+    opacity: .25;
+    background-color: currentColor;
+    animation: spinner-fade 1s linear infinite;
+    .loop(12)
+  }
+  @keyframes spinner-fade {
+    0% {
+      opacity: .85;
+    }
+    50% {
+      opacity: .25;
+    }
+    100% {
+      opacity: .25;
+    }
+  }
+
 </style>
