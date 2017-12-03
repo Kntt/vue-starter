be8ab2d6cee9adce3037611245171961950a3096
kntt <kntt@gmail.com>
2017-12-03 19:49:07 +0800

    update icons

--- src/views/pages/visa/index.vue
+++ src/views/pages/visa/index.vue
@@ -2,6 +2,7 @@
   <div>
     <nav-header title="打卡" fixed>
       <x-button @click="$router.back()" icon="arrow-left" slot="left"></x-button>
+      <x-button icon="calendar" slot="right"></x-button>
     </nav-header>
   </div>
 </template>
